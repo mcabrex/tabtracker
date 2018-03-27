c121c9d66bb2af48f7c32752a92344a44232f6d6
mcabrex <mrcabardo@gmail.com>
2018-03-27 15:58:13 -0400

    added AuthenticationControllers and Vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,22 +1,41 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email"
-    />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password"
-    />
-    <br>
-    <button @click="register">Register</button>
-  </div>
+
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="email"
+          />
+          <br>
+          <input
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="password"
+          />
+          <br>
+          <div  class="error" v-html="error"/>
+          <br>
+          <v-btn 
+            class="cyan"
+            @click="register"
+            >
+            Register
+          </v-btn>
+        </div>
+
+      </div>
+    </v-flex>
+  </v-layout>
+
 </template>
 
 <script>
@@ -25,7 +44,8 @@ export default {
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      error: null
     };
   },
   watch: {
@@ -35,11 +55,15 @@ export default {
   },
   methods: {
     async register(){
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response.data)
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 };
@@ -47,5 +71,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .error {
+    color : red
+  }
 </style>
---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,41 @@
+const Joi = require('joi')
+
+module.exports = {
+  register (req, res, next) {
+    const schema = {
+    // this schema will be used to compare the request against
+      email: Joi.string().email(),
+      // one of Jois built in methods, basically it's making sure it's a string and an email (straightforward enough)
+      password: Joi.string().regex(
+        new RegExp('^[a-zA-Z0-9]{8,32}$')
+        // here we are making sure the password passes a regex requirement of being letters a-zA-Z0-9 and that it should be a minimum of 8 characters and a maximum of 32
+      )
+    }
+    const {error, value} = Joi.validate(req.body, schema)
+    // for Jois validate the first parameter is whatever you're trying to validate and the second parameter is whatever you're trying to validate it against
+
+    if (error) {
+    // here we are going to make a switch to send the appropriate response to whatever caused the error
+      switch (error.details[0].context.key) {
+        case 'email' :
+          res.status(400).send({
+            error: 'You must provide a valid email address'
+          })
+          break
+        case 'password' :
+          res.status(400).send({
+            error: `Password must be between 8 and 32
+                    <br>
+                    Password must contain only letters and numbers`
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid registration information'
+          })
+      }
+    } else {
+      next()
+    }
+  }
+}
