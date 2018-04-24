edc758ade5841ec9d6410fac04003624b34cbcf5
mcabrex <mrcabardo@gmail.com>
2018-04-24 13:59:47 -0400

    fixed some routing problems, added required fields to CreateSong, edited CSS for Songs

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -16,12 +16,37 @@
                     <v-icon>add</v-icon>
                 </v-btn>
 
-            <div 
-                v-for="song in songs" 
+            <div v-for="song in songs" 
+                class="song"
                 :key="song.id">
-                {{song.title}} -
-                {{song.artist}} -
-                {{song.album}} 
+                <v-layout>
+                    <v-flex xs6>
+                        <div class="song-title">
+                            {{song.title}}
+                        </div>
+                        <div class="song-artist">
+                            {{song.artist}}
+                        </div>   
+                        <div class="song-genre">
+                            {{song.genre}}
+                        </div>
+                        <v-btn               
+                        dark
+                        class="cyan"
+                        @click="navigateTo({
+                            name: 'song',
+                            params: {
+                                songId: song.id
+                            }
+                        })">
+                        View
+                        </v-btn>
+                    </v-flex>
+
+                    <v-flex xs6>
+                        <img class="album-image" :src="song.albumImageUrl" />
+                    </v-flex>
+                </v-layout>
             </div>
         </panel>
     </v-flex>
@@ -32,6 +57,11 @@
 import SongsService from '@/services/SongsService'
 import Panel from '@/components/Panel'
 export default {
+    methods : {
+        navigateTo(route) {
+            this.$router.push(route)
+        },
+    },
     components: {
         Panel
     },
@@ -48,5 +78,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.song {
+    padding: 20px;
+    height: 300px;
+    overflow: hidden;
+}
+.song-title {
+    font-size: 30px;
+}
+.song-artist {
+    font-size: 24px;
 
+}
+.song-genre {
+    font-size: 18px;
+ 
+}
+.album-image {
+    width: 70%;
+    margin: 0 auto;
+}
 </style>
