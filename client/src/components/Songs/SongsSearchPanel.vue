a7e44b27680cb577df7fd8a2905f4bffc21464a0
mcabrex <mrcabardo@gmail.com>
2018-05-03 19:56:40 -0400

    added search bar to songs folder that filters from the backend to the fron, installed lodash for search delay

--- client/src/components/Songs/SongsSearchPanel.vue
+++ client/src/components/Songs/SongsSearchPanel.vue
@@ -1,12 +1,40 @@
 <template>
     <panel title="Search">
-
+          <v-text-field
+            label="Search by song title, artist, album, or genre"
+            v-model="search"
+          ></v-text-field>
     </panel>
 </template>
 
 <script>
+import _ from 'lodash'
 export default {
-
+    data () {
+        return {
+            search: ''
+        }
+    },
+    watch: {
+        search: _.debounce(async function(value){
+            const route = {
+                name: 'songs'
+            }
+            if(this.search !== ''){
+                route.query = {
+                    search: this.search
+                }
+            }
+            this.$router.push(route)
+        }, 600),
+        '$route.query.search': {
+            immediate: true,
+            // whenever the route is initialized call whatever our handler is
+            handler(value){
+                this.search = value
+            }
+        }
+    }
 }
 </script>
 
